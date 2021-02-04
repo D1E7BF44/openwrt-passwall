626f0fbcdfd6727d92e8cb0b19a0d752ab1bcfaf
6gfd8 <73461334+6gfd8@users.noreply.github.com>
2021-02-04 13:52:10 +1300

    Update gen_xray_proto.lua

--- luci-app-passwall/luasrc/model/cbi/passwall/api/gen_xray_proto.lua
+++ luci-app-passwall/luasrc/model/cbi/passwall/api/gen_xray_proto.lua
@@ -67,7 +67,7 @@ if server_proto ~= "nil" and server_address ~= "nil" and server_port ~= "nil" th
     if outbound then table.insert(outbounds, outbound) end
 end
 
--- 额外传出连接
+-- Additional outgoing connection
 table.insert(outbounds, {
     protocol = "freedom", tag = "direct", settings = {keep = ""}
 })
@@ -77,11 +77,8 @@ local xray = {
         -- error = string.format("/var/etc/passwall/%s.log", node[".name"]),
         loglevel = "warning"
     },
-    -- 传入连接
     inbounds = inbounds,
-    -- 传出连接
     outbounds = outbounds,
-    -- 路由
     routing = routing
 }
 print(json.stringify(xray, 1))
